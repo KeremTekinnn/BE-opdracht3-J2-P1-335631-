0451a3d7416e4b863ede529d2691beb4e84fcbb9
KeremTekinnn <335631@student.mboutrecht.nl>
2023-10-24 11:27:25 +0200

    fırst commit

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'backend-opdracht-1');
+define('URLROOT', 'http://www.backend-opdracht-2.com');
